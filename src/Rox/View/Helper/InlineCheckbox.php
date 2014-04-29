0f2a356ebe2132d172b59264e6296637a5c10e5e
Marcelo Araújo <marceloaraujo@agenciadeinternet.com>
2014-04-29 10:58:41 -0300

    inline checkbox value

--- src/Rox/View/Helper/InlineCheckbox.php
+++ src/Rox/View/Helper/InlineCheckbox.php
@@ -12,6 +12,6 @@ class InlineCheckbox extends AbstractHelper {
 		if(isset($record[$property]) && $record[$property] != 0){
 			$checked = 'checked="checked"';
 		}
-		return sprintf('<input class="%s" name="%s" type="checkbox" data-id="%s" %s>', $class, $property, $record['_id']->{'$id'}, $checked);
+		return sprintf('<input class="%s" name="%s" type="checkbox" value="1" data-id="%s" %s>', $class, $property, $record['_id']->{'$id'}, $checked);
 	}
 }
